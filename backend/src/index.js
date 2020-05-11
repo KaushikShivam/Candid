638c95a52181fd219437ebf818fced724dd7d969
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-11 14:27:09 +0530

    Configure graphql server with yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,24 @@
-import add from './add';
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(add(2, 4));
+// Type definations
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+	Query: {
+		hello: (parent, args, ctx, info) => `Hello, ${args.name}`,
+	},
+};
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+});
+
+server.start((result) =>
+	console.log(`Server started on port: ${JSON.stringify(result.port)}`)
+);
