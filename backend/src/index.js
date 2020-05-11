060b02ceac0d78ab73155994fd3e89d88e710ce3
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-11 14:46:55 +0530

    Create custom User type

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,25 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definations
 const typeDefs = `
   type Query {
-    hello(name: String): String!
+    user: User!
   }
+  
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+  
 `;
 
 // Resolvers
 const resolvers = {
 	Query: {
-		hello: (parent, args, ctx, info) => `Hello, ${args.name}`,
+		user: (parent, args, ctx, info) => ({
+			id: 1,
+			name: 'shivam',
+			email: 'shivam@gmail.com',
+		}),
 	},
 };
 
